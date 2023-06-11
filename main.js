241216cf3913782bd1fa7bcb415fc07d4fe3329a
DarkPhantom5 <115892067+DarkPhantom5@users.noreply.github.com>
2023-06-11 18:29:28 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+var first_name = " George";
+var last_name = " Scott";
+
+var concat_name = first_name.concat(last_name);
+
+console.log(first_name);
+console.log(last_name);
+console.log(concat_name);
+
+var a = 20;
+var b = 100;
+
+console.log(a);
+console.log(b);
+
+var add = a + b;
+console.log(add);
+
+var multiply = a * b;
+console.log(multiply);
+
+var subtract = b - a;
+console.log(subtract);
+
+var divide = b / a;
+console.log(divide);
